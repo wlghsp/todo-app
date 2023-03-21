d2bcac19e8c066a4a7db282c51052fadea24b48a
jihochoi <wlghsp@gmail.com>
2023-03-21 14:53:47 +0900

    11.5 onToggle, onRemove 함수가 바뀌지 않게 하기
    - 11.5.1  useState의 함수형 업데이트

--- src/App.js
+++ src/App.js
@@ -33,27 +33,27 @@ const App = () => {
                 text,
                 checked: false,
             };
-            setTodos(todos.concat(todo));
+            setTodos(todos => todos.concat(todo));
             nextId.current += 1; // nextId 1씩 더하기
-        }, [todos],
+        }, [],
     );
 
     const onRemove = useCallback(
         id => {
-            setTodos(todos.filter(todo => todo.id !== id));
+            setTodos(todos => todos.filter(todo => todo.id !== id));
         },
-        [todos],
+        [],
     );
 
     const onToggle = useCallback(
         id => {
-            setTodos(
+            setTodos(todos =>
                 todos.map(todo =>
                     todo.id === id ? {...todo, checked: !todo.checked} : todo,
                 ),
             );
         },
-        [todos],
+        [],
     );
 
 
